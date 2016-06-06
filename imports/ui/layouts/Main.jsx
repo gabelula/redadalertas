d612ab62b7e064943cea06d925a55f710fcb0ed1
Celso Mireles <celso@digitalstrategy.tips>
2016-06-05 20:18:12 -0700

    Add OG meta tags

--- imports/ui/layouts/Main.jsx
+++ imports/ui/layouts/Main.jsx
@@ -8,6 +8,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MaterialTitlePanel from '../components/MaterialTitlePanel.jsx';
 import { TAPi18n } from 'meteor/tap:i18n';
 import LanguagePicker from '../components/LanguagePicker';
+import DocMeta from 'react-doc-meta';
 
 
 const styles = {
@@ -90,11 +91,31 @@ export default class Main extends TrackerReact(Component) {
       onSetOpen: this.onSetOpen.bind(this),
     };
 
+		var tags = [
+      {name: "description", content: "Un mapa de las redadas que el partido Democrata patroniza."},
+      {itemProp: "name", content: "Redadas De Obama"},
+      {itemProp: "description", content: "Un mapa de las redadas que el partido Democrata y Obama patronizan."},
+      {itemProp: "image", content: "https://dl.dropboxusercontent.com/u/16972085/cosecha/160513-redadas-de-obama.jpg"},
+      {name: "twitter:card", content: "site"},
+      {name: "twitter:site", content: "@cosecha_harvest"},
+      {name: "twitter:title", content: "Redadas De Obama"},
+      {name: "twitter:description", content: "Un mapa de las redadas que el partido Democrata y Obama patronizan."},
+      {name: "twitter:creator", content: "@author_handle"},
+      {name: "twitter:image", content: "https://dl.dropboxusercontent.com/u/16972085/cosecha/160513-redadas-de-obama.jpg"},
+      {property: "og:title", content: "Redadas De Obama"},
+      {property: "og:type", content: "site"},
+      {property: "og:url", content: "http://www.redadasdeobama.com"},
+      {property: "og:image", content: "https://dl.dropboxusercontent.com/u/16972085/cosecha/160513-redadas-de-obama.jpg"},
+      {property: "og:description", content: "Un mapa de las redadas que el partido Democrata y Obama patronizan."},
+      {property: "og:site_name", content: "Redadas de Obama"}
+    ]
+
     return (
 			<MuiThemeProvider muiTheme={getMuiTheme()}>
 				<Sidebar {...sidebarProps}>
 	        <MaterialTitlePanel title={contentHeader}>
 	          <div style={styles.content}>
+							<DocMeta tags={tags} />
 							{this.props.yield}
 
 	          </div>
